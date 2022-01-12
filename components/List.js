f19f30569530793e3946747413d15289c10d8ee7
R <ramz9_97@hotmail.com>
2022-01-12 11:26:45 +0200

    added custom hook, task http c done

--- components/List.js
+++ components/List.js
@@ -1,37 +1,10 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {FlatList} from 'react-native';
-import {baseUrl} from '../utils/variables';
+import {useMedia} from '../hooks/ApiHooks';
 import ListItem from './listItem';
 
 const List = () => {
-  const [mediaArray, setMediaArray] = useState([]);
-  // let mediaArray = [];
-  const loadMedia = async (start = 0, limit = 10) => {
-    try {
-      const response = await fetch(
-        `${baseUrl}media?start=${start}&limit=${limit}`
-      );
-      if (!response) {
-        throw Error(response.statusText);
-      }
-      const json = await response.json();
-      const media = await Promise.all(
-        json.map(async (item) => {
-          const response = await fetch(baseUrl + 'media/' + item.file_id);
-          const mediaData = await response.json();
-          return mediaData;
-        })
-      );
-      setMediaArray(media);
-    } catch (e) {
-      console.error(e);
-    }
-    console.log(mediaArray);
-  };
-
-  useEffect(() => {
-    loadMedia(0, 5);
-  }, []);
+  const {mediaArray} = useMedia();
 
   return (
     <FlatList
---
+++ hooks/ApiHooks.js
@@ -0,0 +1,36 @@
+import {useEffect, useState} from 'react';
+import {baseUrl} from '../utils/variables';
+
+const useMedia = () => {
+  const [mediaArray, setMediaArray] = useState([]);
+  const loadMedia = async (start = 0, limit = 10) => {
+    try {
+      const response = await fetch(
+        `${baseUrl}media?start=${start}&limit=${limit}`
+      );
+      if (!response) {
+        throw Error(response.statusText);
+      }
+      const json = await response.json();
+      const media = await Promise.all(
+        json.map(async (item) => {
+          const response = await fetch(baseUrl + 'media/' + item.file_id);
+          const mediaData = await response.json();
+          return mediaData;
+        })
+      );
+      setMediaArray(media);
+    } catch (e) {
+      console.error(e);
+    }
+    console.log(mediaArray);
+  };
+
+  useEffect(() => {
+    loadMedia(0, 5);
+  }, []);
+
+  return {mediaArray};
+};
+
+export {useMedia};
