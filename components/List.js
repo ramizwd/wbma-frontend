aeba49f89e01698b5936f88aea39ce975d1ed104
R <ramz9_97@hotmail.com>
2022-01-12 11:04:43 +0200

    fetch data from api, task http b done

--- components/List.js
+++ components/List.js
@@ -1,35 +1,42 @@
 import React, {useEffect, useState} from 'react';
 import {FlatList} from 'react-native';
+import {baseUrl} from '../utils/variables';
 import ListItem from './listItem';
-
-const dataUrl =
-  'https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json';
 
 const List = () => {
   const [mediaArray, setMediaArray] = useState([]);
   // let mediaArray = [];
-  const loadMedia = async () => {
+  const loadMedia = async (start = 0, limit = 10) => {
     try {
-      const response = await fetch(dataUrl);
+      const response = await fetch(
+        `${baseUrl}media?start=${start}&limit=${limit}`
+      );
       if (!response) {
         throw Error(response.statusText);
       }
       const json = await response.json();
-      setMediaArray(json);
-      console.log(mediaArray);
+      const media = await Promise.all(
+        json.map(async (item) => {
+          const response = await fetch(baseUrl + 'media/' + item.file_id);
+          const mediaData = await response.json();
+          return mediaData;
+        })
+      );
+      setMediaArray(media);
     } catch (e) {
       console.error(e);
     }
+    console.log(mediaArray);
   };
 
   useEffect(() => {
-    loadMedia();
+    loadMedia(0, 5);
   }, []);
 
   return (
     <FlatList
       data={mediaArray}
-      keyExtractor={(item) => item.title}
+      keyExtractor={(item) => item.file_id.toString()}
       renderItem={({item}) => <ListItem singleMedia={item} />}
     ></FlatList>
   );
